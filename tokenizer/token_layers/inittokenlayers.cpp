50144058be0c5d58efbeb213b6bd20bca5698fc1
ilor <ilor@ilor-laptop.(none)>
2010-03-30 13:39:27 +0200

    caseless lexicon layer and tests

--- tokenizer/token_layers/inittokenlayers.cpp
+++ tokenizer/token_layers/inittokenlayers.cpp
@@ -6,18 +6,29 @@
 #include "regexpclassifylayer.h"
 #include "lexiconclassifylayer.h"
 
+/*
+template <class T, bool T::mptr(const T&)>
+bool WRAP(const T& t1, const T& t2)
+{
+	return t1.*mptr(t2);
+}
+*/
+
+#include <boost/bind.hpp>
+
 bool init_token_layers()
 {
 	static bool init_done = false;
 	if (init_done) return false;
-
+	//boost::bind(&UnicodeString::compare, _1, _2);
 	TokenLayer::register_layer<TokenLayer>("passthrough");
 	TokenLayer::register_layer<AffixSplitLayer>("affix_split");
 	TokenLayer::register_layer<AppendLayer>("append");
 	TokenLayer::register_layer<BasicSplitLayer>("split");
 	TokenLayer::register_layer<CombineLayer>("combine");
 	TokenLayer::register_layer<RegexpClassifyLayer>("regexp");
-	TokenLayer::register_layer<LexiconClassifyLayer>("lexicon");
+	TokenLayer::register_layer<CaseLexiconClassifyLayer>("lexicon");
+	TokenLayer::register_layer<CaselessLexiconClassifyLayer>("lexicon_caseless");
 
 	init_done = true;
 	return true;
--- tokenizer/token_layers/lexiconclassifylayer.h
+++ tokenizer/token_layers/lexiconclassifylayer.h
@@ -7,6 +7,7 @@
 
 #include <set>
 
+template <typename CMP>
 class LexiconClassifyLayer : public TokenLayer
 {
 public:
@@ -17,8 +18,62 @@ public:
 	Token* processToken(Token *t);
 
 private:
-	std::set<UnicodeString> lex_;
+	std::set<UnicodeString, CMP> lex_;
 	std::string token_type_;
 };
+
+
+#include "token.h"
+#include <boost/algorithm/string.hpp>
+#include <boost/foreach.hpp>
+
+
+template<typename CMP>
+LexiconClassifyLayer<CMP>::LexiconClassifyLayer(TokenSource *input, const Config::Node &props)
+	: TokenLayer(input, props), lex_(), token_type_()
+{
+	token_type_ = props.get<std::string>("token_type", "lex");
+	std::vector<std::string> v;
+	std::string sep(", ");
+	const std::string& sepstring = props.get<std::string>("lexicon", "");
+	boost::algorithm::split(v, sepstring, boost::is_any_of(sep));
+	BOOST_FOREACH (const std::string& s, v) {
+		if (!s.empty()) {
+			lex_.insert(UnicodeString::fromUTF8(s).unescape());
+		}
+	}
+}
+
+template<typename CMP>
+LexiconClassifyLayer<CMP>::~LexiconClassifyLayer()
+{
+}
+
+template<typename CMP>
+Token* LexiconClassifyLayer<CMP>::processToken(Token *t)
+{
+	if (lex_.find(t->orth()) != lex_.end()) {
+		t->set_type(token_type_);
+	}
+	return t;
+}
+
+struct IcuStringCaselessCompare
+{
+public:
+	bool operator()(const UnicodeString& u1, const UnicodeString& u2) {
+		return u1.caseCompare(u2, 0) < 0;
+	}
+};
+
+typedef LexiconClassifyLayer<
+	std::less< UnicodeString >
+> CaseLexiconClassifyLayer;
+
+typedef LexiconClassifyLayer<
+	IcuStringCaselessCompare
+> CaselessLexiconClassifyLayer;
+
+
 
 #endif // LEXICONCLASSIFYLAYER_H
