57a2920eb8da670f8284b1f17b77704237c0e8bc
ilor <ilor@eeelor.(none)>
2010-04-04 19:55:20 +0200

    doxify tokenizer debug functions

--- tokenizer/debug.h
+++ tokenizer/debug.h
@@ -10,30 +10,58 @@ class TokenSource;
 
 namespace Debug {
 
+	/**
+	 * Helper function to pass a token through a given function and feed the
+	 * resulting std::strings into an ostream
+	 */
 	void sink_to_stream(boost::function<std::string (const Token&)> func, std::ostream& os, Token* t);
 
+	/**
+	 * Tokenize the tokens from the TokenSource by passing all tokens through
+	 * the given functon and concatenating the output std::strings.
+	 */
 	std::string tokenize(TokenSource& tok, boost::function<std::string (const Token&)> func);
 
-	std::string token_format(const std::string& format, const Token& t);
-
-	std::string token_debug(const Token& t);
-
-	std::string tokenize_orths_newline(TokenSource& ts);
-
-	void tokenize_orths_newline(TokenSource& ts, std::ostream& os);
-
 	/**
-	 * Return a string with all tokens processed and printed according to the
-	 * given format string. The following replacements will be done:
+	 * Formatted token debug output. The following replacements will be done:
 	 * - $orth   -> the token's orth
 	 * - $type   -> the token's type
 	 * - $ws     -> the preceeding whitespace amount string representation
 	 *              e.g. "none". @see Token::WhitespaceAmount
 	 * - $ws_id  -> the preceeding whitespace numeric code, 0 for no whtespace
 	 * - $ws_any -> 0 if there was any whitespace preceeding the token, else 1
+	 * The format string is unescaped so e.g. backslash-n is a valid way of
+	 * putting a newline into the debug output.
+	 */
+	std::string token_format(const std::string& format, const Token& t);
+
+	/**
+	 * "Standard" token debug output
+	 */
+	std::string token_debug(const Token& t);
+
+	/**
+	 * "Standard" debug tokenization -- orths with newline, equivalent to a
+	 * $orth\n format.
+	 */
+	std::string tokenize_orths_newline(TokenSource& ts);
+
+	/**
+	 * "Standard" debug tokenization -- orths with newline, equivalent to a
+	 * $orth\n format.
+	 */
+	void tokenize_orths_newline(TokenSource& ts, std::ostream& os);
+
+	/**
+	 * Return a string with all tokens processed and printed according to the
+	 * given format string, @see token_format for format options.
 	 */
 	std::string tokenize_formatted(TokenSource& tok, const std::string& format);
 
+	/**
+	 * Output into the stream all tokens processed and printed according to the
+	 * given format string, @see token_format for format options.
+	 */
 	void tokenize_formatted(TokenSource& tok, const std::string& format, std::ostream& os);
 
 } //end namespace Debug
--- tokenizer/util.h
+++ tokenizer/util.h
@@ -6,6 +6,11 @@
 
 namespace Util {
 
+/**
+ * Helper function to put all characters from a std::string and put them in a
+ * (set-like) container. The string is converted to a UnicodeString, unescaping
+ * is performed and UChars are fed into the container.
+ */
 template <typename TContainer>
 void utf8StringToUcharContainer(const std::string& s, TContainer& container)
 {
@@ -15,6 +20,10 @@ void utf8StringToUcharContainer(const std::string& s, TContainer& container)
 	}
 }
 
+/**
+ * Helper function to 'unescape' an UTF8 string, which is done by converting
+ * to a UnicodeString, ICU-unescaping and converting back to UTF8.
+ */
 std::string unescape_utf8(const std::string& str);
 
 } //end namespace Util
