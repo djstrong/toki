4b35a5e324c0e2f83ba53fd51aedf53c05eae736
ilor <kailoran@gmail.com>
2010-04-22 22:19:36 +0200

    .so message tweaks

--- libtoki/main.cpp
+++ libtoki/main.cpp
@@ -1,24 +1,42 @@
 /* Code to display version info and greeting message when the .so is run */
+
 #ifdef __GNUG__
 #ifndef _WIN32
+
+#ifndef LIBTOKI_INTERP
 #ifdef __LP64__
-extern "C" const char my_interp[] __attribute__((section(".interp"))) = "/lib/ld-linux-x86-64.so.2";
+#define LIBTOKI_INTERP "/lib/ld-linux-x86-64.so.2";
 #else
-extern "C" const char my_interp[] __attribute__((section(".interp"))) = "/lib/ld-linux.so.2";
+#define LIBTOKI_INTERP "/lib/ld-linux.so.2";
+#endif
+#endif
+
+#ifdef LIBTOKI_INTERP
+extern "C"
+const char my_interp[] __attribute__((section(".interp"))) = LIBTOKI_INTERP;
 #endif
 
 #include "config_d.h"
+#include "tokenlayer.h"
+#include "token_layers/inittokenlayers.h"
+#include "layertokenizer.h"
 #include <cstdlib>
 #include <iostream>
+#include <boost/algorithm/string.hpp>
 
 #ifndef LIBTOKI_VERSION
 #define LIBTOKI_VERSION "?"
 #endif
 
-extern "C" int LIBTOKI_entry_()
+extern "C"
+int LIBTOKI_entry_()
 {
-	std::cout << "This is libtoki-" LIBTOKI_VERSION "\n";
+	std::cout << "This is libtoki-" LIBTOKI_VERSION ", a configurable tokenization library.\n";
 	std::cout << "Default config file path: " LIBTOKI_DATA_DIR "\n";
+	Toki::init_token_layers();
+	std::cout << "Available layer types: "
+		<< boost::algorithm::join(Toki::TokenLayer::available_layer_types(), " ")
+		<< "\n";
 	exit(0);
 }
 
