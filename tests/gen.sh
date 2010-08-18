1a3dd5add839bacf96a500cf7b28395a52a36792
ilor <ilor@ilor-laptop.(none)>
2010-08-18 14:28:53 +0200

    sanity tests update -- add some latin2 characters to generator, generate some files, add wikipedia data

--- tests/gen.sh
+++ tests/gen.sh
@@ -4,11 +4,17 @@ if [ "$1" == "" ]; then
 else
 	BYTES=$1
 fi
-tr -dc "[:cntrl:]A-Za-z0-9.,!()[] \n{}\"';:" </dev/urandom | \
+ASC="tr -dc \"[:cntrl:]A-Za-z0-9.,!()[]\\n{}\\\"\';:\""
+if [ "$2" == "pl" ]; then
+	ASC="tr -dc \"[:cntrl:][:print:][\\241-\\377\""
+fi
+cat /dev/urandom | \
+	$ASC | \
 	tr "[:cntrl:]" " " | \
 	head -c $BYTES | \
 	sed "s/   /\n/g" | \
 	sed "s/^ \+//" | \
-	sed "s/ +$//" | \
+	sed "s/ \+$//" | \
 	sed "/^$/d" | \
-	sed "\${/^$/!s/$/\n/;}"
+	sed "\${/^$/!s/$/\n/;}" | \
+	iconv -c -f latin2 -t utf8//TRANSLIT
